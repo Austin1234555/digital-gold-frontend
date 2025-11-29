f9fc4ed2397f32bdebfea23c08028c1b16d0f978
Austin <austinedwin761@gmail.com>
2025-11-29 19:40:45 +0530

    updated3

--- src/services/api.js
+++ src/services/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const API_BASE = "https://digital-gold-backend.onrender.com";
 
 export const signup = (name, email, password) =>
-  axios.post(`${API_BASE}/auth/signup?name=${name}&email=${email}&password=${password}`);
+  axios.post(`${API_BASE}/auth/signup`, { name, email, password });
 
 export const login = (email, password) =>
-  axios.post(`${API_BASE}/auth/login?email=${email}&password=${password}`);
+  axios.post(`${API_BASE}/auth/login`, { email, password });
 
 export const getWallet = (userId) =>
   axios.get(`${API_BASE}/wallet/${userId}`);
@@ -15,7 +15,7 @@ export const getLatestPrice = () =>
   axios.get(`${API_BASE}/price/latest`);
 
 export const createRazorpayOrder = (userId, amount, method) =>
-  axios.post(`${API_BASE}/razorpay/create-order?userId=${userId}&amount=${amount}&method=${method}`);
+  axios.post(`${API_BASE}/razorpay/create-order`, { userId, amount, method });
 
 export const confirmRazorpayPayment = (data) =>
   axios.post(`${API_BASE}/razorpay/confirm-payment`, data);
